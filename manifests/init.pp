aff849dfe738eebbff2be4de6d431e2e18192da6
Alban Peignier <alban@tryphon.eu>
2011-07-23 16:26:22 +0200

    Initial import

---
+++ manifests/init.pp
@@ -0,0 +1,25 @@
+class shorewall {
+
+  package { shorewall: ensure => latest }
+  
+  file { "/etc/shorewall":
+    ensure => directory,
+    source => "puppet:///files/shorewall/$fqdn",
+    require => Package[shorewall],
+    recurse => true,
+    notify => Service[shorewall]
+  }
+
+  file { "/etc/default/shorewall":
+    source => "puppet:///shorewall/shorewall.default",
+    notify => Service[shorewall]
+  }
+
+  service { shorewall:
+    ensure => running,
+    require => Package[shorewall],
+    hasrestart => true,
+    status => '/sbin/shorewall status'
+  }
+
+}
